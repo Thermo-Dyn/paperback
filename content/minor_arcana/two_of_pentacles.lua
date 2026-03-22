4ec98c6e858c837f8ec6c154f8d86945aa72d53b
TheSnaz <Snaz.Dev@protonmail.com>
2026-03-22 23:05:29 +0100

    (2 of Pentacles) No longer usable on permanently eternal cards

--- content/minor_arcana/two_of_pentacles.lua
+++ content/minor_arcana/two_of_pentacles.lua
@@ -26,12 +26,14 @@ PB_UTIL.MinorArcana {
 
       -- checks for non-compatible jokers that have eternal anyway
       -- just in case :P
-      return joker.config.center.eternal_compat or joker.ability.eternal
+      return (joker.config.center.eternal_compat or joker.ability.eternal) and
+          not (joker.config.center.paperback.permanently_eternal)
     end
   end,
 
   keep_on_use = function(self, card)
-    return G.jokers.highlighted[1].config.center.paperback and G.jokers.highlighted[1].config.center.paperback.permanently_eternal
+    return G.jokers.highlighted[1].config.center.paperback and
+        G.jokers.highlighted[1].config.center.paperback.permanently_eternal
   end,
 
   use = function(self, card)
