30addb37706e15095de13df16710d3446da6261e
srockw <136873475+srockw@users.noreply.github.com>
2026-09-13 23:01:16 -0300

    (Black Knife) Fix crash & logic to select joker
    - Update to using suit shades
    
    - Previously it'd get all possible jokers for each card scored, now it gets all jokers outside the loop, which prevents the chance of trying to destroy the same joker twice

--- content/joker/black_knife.lua
+++ content/joker/black_knife.lua
@@ -27,8 +27,10 @@ SMODS.Joker {
   },
 
   loc_vars = function(self, info_queue, card)
-    info_queue[#info_queue + 1] = PB_UTIL.suit_tooltip('light')
     local numerator, denominator = PB_UTIL.chance_vars(card)
+
+    info_queue[#info_queue + 1] = PB_UTIL.suit_tooltip('light')
+
     return {
       vars = {
         card.ability.extra.destroy, card.ability.extra.left, numerator, denominator
@@ -37,7 +39,12 @@ SMODS.Joker {
   end,
 
   locked_loc_vars = function(self, info_queue, card)
-    return { vars = { 100, PB_UTIL.get_career_stat("played_stars", 0) } }
+    return {
+      vars = {
+        100,
+        PB_UTIL.get_career_stat("played_stars", 0)
+      }
+    }
   end,
 
   check_for_unlock = function(self, args)
@@ -47,20 +54,22 @@ SMODS.Joker {
   calculate = function(self, card, context)
     if context.remove_playing_cards then
       for _, v in ipairs(context.removed or {}) do
-        if PB_UTIL.is_suit(v, 'light') then
+        if v:is_suit_shade("light") then
           card.ability.extra.left = card.ability.extra.left - 1
+
           if card.ability.extra.left <= 0 then
             card.ability.extra.left = card.ability.extra.destroy
+
             G.E_MANAGER:add_event(Event {
               trigger = 'immediate',
               func = function()
-                  SMODS.add_card({
-                    set = 'Joker',
-                    area = G.jokers,
-                    edition = 'e_negative',
-                  })
+                SMODS.add_card({
+                  set = 'Joker',
+                  area = G.jokers,
+                  edition = 'e_negative',
+                })
                 return true
-              end,
+              end
             })
           end
         end
@@ -68,23 +77,27 @@ SMODS.Joker {
     end
 
     if context.after and not context.blueprint then
+      local possible_jokers = {}
+      for _, j in ipairs(G.jokers.cards) do
+        if j ~= card and not SMODS.is_eternal(j, card) and not j.getting_sliced then
+          possible_jokers[#possible_jokers + 1] = j
+        end
+      end
+
       for _, v in ipairs(context.scoring_hand) do
-        if PB_UTIL.is_suit(v, 'light') and PB_UTIL.chance(card, "paperback_SWOON") then
-          local possible_jokers = {}
-            for _, j in ipairs(G.jokers.cards) do
-              if j ~= card and not SMODS.is_eternal(j, card) and not j.getting_sliced then possible_jokers[#possible_jokers + 1] = j end
-            end
+        if v:is_suit_shade("light") and PB_UTIL.chance(card, "paperback_SWOON") then
+          local joker_to_destroy, index = pseudorandom_element(possible_jokers, 'paperback_SWOON')
 
-            local joker_to_destroy = pseudorandom_element(possible_jokers, pseudoseed('paperback_SWOON'))
+          if joker_to_destroy then
+            table.remove(possible_jokers, index)
+            PB_UTIL.destroy_joker(joker_to_destroy)
 
-            if joker_to_destroy then
-              PB_UTIL.destroy_joker(joker_to_destroy)
-              SMODS.calculate_effect {
-                message = localize('paperback_swoon_ex'),
-                colour = G.C.PAPERBACK_DARK_SUIT,
-                card = card,
-              }
-            end
+            SMODS.calculate_effect {
+              message = localize('paperback_swoon_ex'),
+              colour = G.C.PAPERBACK_DARK_SUIT,
+              card = card,
+            }
+          end
         end
       end
     end
